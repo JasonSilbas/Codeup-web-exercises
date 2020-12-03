b53a5b4c5f5997899f1e591ab9cb3e44c3a075ac
Jason Silbas <silbasjason25@gmail.com>
2020-12-03 17:06:24 -0600

    update

---
+++ while.js
@@ -0,0 +1,10 @@
+
+
+var names = ['jason', 'cristina', 'pearl', 'ruby'];
+    console.log(names.length);
+
+    console.log(name[0]);
+    console.log(name[1]);
+    console.log(name[2]);
+    console.log(name[3]);
+    console.log(name[4]);
